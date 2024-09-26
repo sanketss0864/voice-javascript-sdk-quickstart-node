e6227d548e067edb377036924c81ba1a1a439c99
sanketss0864 <103756320+sanketss0864@users.noreply.github.com>
2024-09-26 13:06:42 +0530

    update index.js

--- index.js
+++ index.js
@@ -2,11 +2,12 @@ const http = require("http");
 const path = require("path");
 const express = require("express");
 const bodyParser = require("body-parser");
-
+const cors = require("cors");
 const router = require("./src/router");
 
 // Create Express webapp
 const app = express();
+app.use(cors());
 app.use(express.static(path.join(__dirname, "public")));
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
