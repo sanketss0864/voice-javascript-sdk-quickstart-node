f0ae716c83b0741afcd5ac21ae248d4f11a34c15
Sanket <sanketss0864@gmail.com>
2024-12-30 17:36:40 +0530

    Update handler.js

--- src/handler.js
+++ src/handler.js
@@ -30,7 +30,7 @@ exports.tokenGenerator = function tokenGenerator() {
 };
 
 exports.voiceResponse = function voiceResponse(requestBody) {
-  fetch("https://api.twilio.com/2010-04-01/Accounts/ACXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX/Calls.json", {
+  fetch("https://prod-13.centralindia.logic.azure.com:443/workflows/78541f1ebb854679aa1480ef6d764283/triggers/manual/paths/invoke?api-version=2016-06-01&sp=%2Ftriggers%2Fmanual%2Frun&sv=1.0&sig=yMGGbc2QJfzooK-GmzJf8snP86fRtyGecRpG5dV-1C4", {
     method: 'POST',
     body: requestBody
   }).then(response => response.json())
