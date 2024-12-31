bfffc426444994b77df7d9f0156ab78bf26722f5
Sanket <sanketss0864@gmail.com>
2024-12-31 13:33:55 +0530

    Update handler.js

--- src/handler.js
+++ src/handler.js
@@ -30,16 +30,16 @@ exports.tokenGenerator = function tokenGenerator() {
 };
 
 exports.voiceResponse = function voiceResponse(requestBody) {
-fetch("https://prod-13.centralindia.logic.azure.com:443/workflows/78541f1ebb854679aa1480ef6d764283/triggers/manual/paths/invoke?api-version=2016-06-01&sp=%2Ftriggers%2Fmanual%2Frun&sv=1.0&sig=yMGGbc2QJfzooK-GmzJf8snP86fRtyGecRpG5dV-1C4", {
-  method: 'POST',
-  headers: {
-    'Content-Type': 'application/json'
-  },
-  body: requestBody
-})
-.then(response => response)
-.then(json => console.log(json))
-.catch(error => console.error('Error:', error));
+  fetch("https://prod-13.centralindia.logic.azure.com:443/workflows/78541f1ebb854679aa1480ef6d764283/triggers/manual/paths/invoke?api-version=2016-06-01&sp=%2Ftriggers%2Fmanual%2Frun&sv=1.0&sig=yMGGbc2QJfzooK-GmzJf8snP86fRtyGecRpG5dV-1C4", {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(requestBody) // Properly stringify the request body
+  })
+  .then(response => response.json())
+  .then(json => console.log(json))
+  .catch(error => console.error('Error:', error));
 
   const toNumberOrClientName = requestBody.To;
   const callerId = config.callerId;
