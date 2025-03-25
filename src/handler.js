a6d238313a598cd7525e34966286dc9bd8358f3b
Sanket <sanketss0864@gmail.com>
2025-03-25 18:53:05 +0530

    added code for transcription

--- src/handler.js
+++ src/handler.js
@@ -94,14 +94,15 @@ exports.recordResponse = function recordResponse(requestBody) {
     dial.client(identity);
     twiml.say("hi Thanks for calling!");
   } else if (requestBody.To) {
-    // This is an outgoing call
 
-    // set the callerId
-    let dial = twiml.dial({     record: 'record-from-ringing-dual',
-      recordingStatusCallback: 'https://prod-24.centralindia.logic.azure.com:443/workflows/962952e47cf24bb997f980ce15be6e31/triggers/manual/paths/invoke?api-version=2016-06-01&sp=%2Ftriggers%2Fmanual%2Frun&sv=1.0&sig=uhO1TzOinxcVA2ry4kmsz3iYclO7RBZxGq3Nje3jyhI',callerId });
+    let dial = twiml.dial({
+      record: 'record-from-ringing-dual',
+      recordingStatusCallback: 'https://prod-24.centralindia.logic.azure.com:443/workflows/962952e47cf24bb997f980ce15be6e31/triggers/manual/paths/invoke?api-version=2016-06-01&sp=%2Ftriggers%2Fmanual%2Frun&sv=1.0&sig=uhO1TzOinxcVA2ry4kmsz3iYclO7RBZxGq3Nje3jyhI', callerId
+    });
 
-    // Check if the 'To' parameter is a Phone Number or Client Name
-    // in order to use the appropriate TwiML noun 
+    const start = twiml.start();
+    start.transcription({statusCallbackUrl: 'https://prod-24.centralindia.logic.azure.com:443/workflows/962952e47cf24bb997f980ce15be6e31/triggers/manual/paths/invoke?api-version=2016-06-01&sp=%2Ftriggers%2Fmanual%2Frun&sv=1.0&sig=uhO1TzOinxcVA2ry4kmsz3iYclO7RBZxGq3Nje3jyhI', name: 'Contact center transcription'})
+
     const attr = isAValidPhoneNumber(toNumberOrClientName)
       ? "number"
       : "client";
